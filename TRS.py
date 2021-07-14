744a43df879f01932729c5f5ae9e1fbfbfce388e
mahtab-sajid <87368480+mahtab-sajid@users.noreply.github.com>
2021-07-14 10:26:57 +0600

    Love TRS

---
+++ TRS.py
@@ -0,0 +1,12 @@
+CYBER SECURITY................
+
+TTTTTTTT                  SSSSSSS
+        T       RRRR          S
+        T       R         R      SSSSSSS
+        T       R         R                    S
+        T       RRRRR                      S
+                 R          R      SSSSSSS
+                 R            R            
+                 R              R
+
+🎭Developed By:-Mahtab🎭
